e0ca29e6b7828707626b3c98e12861eccc56aae3
Mackenzie Berliner-Glasser <mack.bg94@gmail.com>
2019-05-07 15:25:37 -0700

    added map functionality and updated readme for students

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-# This is an overview of some ES6 as a refresher because why not! 
+# This is an overview of some ES6 as a refresher because why not!
+
+## A utility for students at ALC to look at examples of some array methods as well as other es6 examples.
+
+### Array Methods:
+- `arr.forEach`
+- `arr.map`
--- src/map.js
+++ src/map.js
@@ -1,7 +1,23 @@
+// map doesnt mutate an array but returns a new array
+// it is also an iterator that allows us to do a variety of things to arrays!
+
+// double all numbers in an array;
 const arr = [1, 2, 3, 4];
+//const doubleArr = arr.map(num => num * 2);
 
 const double = num => num * 2;
-
 const doubleArr = arr.map(double);
 console.log(doubleArr);
+
+// return an array of a specific property of of an object.
+const cars = [
+  { model: 'nissan', make: 'sentra', price: 3000 },
+  { model: 'mazda', make: 'cx3', price: 8000 },
+  { model: 'toyota', make: 'carolla', price: 5000 }
+];
+
+const prices = cars.map(car => car.price);
+console.log(prices);
+
+
 
