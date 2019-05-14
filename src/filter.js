059d27d0c3c1beccaa27e8af7ec9308167eb55fa
Mackenzie Berliner-Glasser <mack.bg94@gmail.com>
2019-05-13 21:42:06 -0700

    added two challenges 4 the kidz

--- src/filter.js
+++ src/filter.js
@@ -22,3 +22,42 @@ const xtremeFiltered = pets.filter(
   pet => pet.type === 'turtle' && pet.weight > 35 && pet.age > 100
 );
 console.log('xtreme!!', xtremeFiltered);
+
+//practical blog example:
+
+//single post objects
+const post = { id: 4, title: 'IMPORTANT' };
+
+//array of comments for all posts
+const comments = [
+  { postId: 4, content: 'this is stupid' },
+  { postId: 3, content: 'i love beyonce' },
+  { postId: 4, content: 'how dare you' },
+  { postId: 2, content: 'ew' }
+];
+
+const commentsForPost = (post, comments) => {
+  return comments.filter(comment => comment.postId === post.id);
+};
+//returns an array of objects only for a post where the post id matches.
+console.log(commentsForPost(post, comments));
+
+/*
+EXAMPLES:
+
+  1. create an array of user objects with an id property and a admin property that is a boolean.
+    -filter the array of users objects so you only return those who have admin access.
+
+  2. **HARDER** create a function reject that takes an array and a function
+    -reject should work the opposite of filter.
+     If the function returns true the item should **NOT** be included in the array.
+
+    EX:
+
+    const numbers = [10, 20, 30];
+    const lessThanFifteen = reject(numbers, fn(number) {
+      return number > 15;
+    });
+    lessThanFifteen // [ 10 ];
+
+*/
