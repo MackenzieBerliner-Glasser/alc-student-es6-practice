22b43f5a769888e78fce75ec09c7959fe8f23a34
Mackenzie Berliner-Glasser <mack.bg94@gmail.com>
2019-05-17 14:20:52 -0700

    added some exercises

--- src/array-helpers/find.js
+++ src/array-helpers/find.js
@@ -1,10 +1,23 @@
 const users = [
-  { name: 'ben' },
-  { name: 'jeff' },
-  { name: 'laura' },
-  { name: 'jean' }
+  { name: 'ben', admin: false },
+  { name: 'jeff', admin: true },
+  { name: 'laura', admin: true },
+  { name: 'jean', admin: false }
 ];
 
+//find is an iterator that finds the ***FIRST*** element that returns true
 const user = users.find(user => user.name === 'laura');
 console.log(user);
+
+const admin = users.find(user => user.admin === true);
+//do you think jeff or laura will be returned as admin?
+console.log(admin);
+/*
+  Exercises:
+    1. given an account array of objects that have user and balance properties find the balance of your choosing.
+    2. ***CHALLENGE*** write a function findWhere() that takes an array as well as a criteria and returns the property searched
+
+    EX: findWhere(array, { key: value })
+*/
+
 
