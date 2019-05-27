f23e2920b59e5d7b98c3f80ab0a5e7d7034a1f30
Mackenzie Berliner-Glasser <mack.bg94@gmail.com>
2019-05-27 13:45:58 -0700

    added exercises for reduce.

--- src/array-helpers/reduce.js
+++ src/array-helpers/reduce.js
@@ -3,4 +3,36 @@ const numArr = [1, 2, 3, 4, 5];
 const sum = arr => arr.reduce((accum, curr) => accum + curr, 0);
 console.log(sum(numArr));
 
+/*
+  write a function that checks if you have balanced parens.
+  `()((()))` true
+  `)()(` false *edge case*
+  `((((()))))` true
+  `))))` false
+*/
+
+const balancedParens = string => {
+  return !string.split('').reduce((prev, char) => {
+    if(prev < 0) return prev;
+    if(char === '(') return prev ++;
+    if(char === ')') return prev --;
+    return prev;
+  }, 0);
+};
+console.log(balancedParens('()()()'));
+
+
+/*
+  given an array of objects
+
+  const desks = [
+    { type: 'sitting},
+    { type: 'standing}
+  ];
+
+  use reduce in a function deskTypes that returns an object that tracks the count of desk types
+
+  EX: { sitting: 1, standing: 1};
+
+*/
 
