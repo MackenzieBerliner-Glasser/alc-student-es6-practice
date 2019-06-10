1c57e089720f746bd85151e16f0311490ca6b99a
Mackenzie Berliner-Glasser <mack.bg94@gmail.com>
2019-06-10 15:46:09 -0700

    const vs let added

--- src/const-vs-let/const-vs-let.js
+++ src/const-vs-let/const-vs-let.js
@@ -32,3 +32,65 @@ console.log(name) // daniel
 
 // now with ES6 we can be more specific about how we declare variables.
 
+// Big difference between var and let is that let is block scoped while var is function scoped.
+
+function names(names) {
+  var newNames = [];
+  for(var i = 0; i < names.length; i++) {
+    const name = names[i];
+    newNames.push(`my name is ${name}`);
+  }
+  console.log(i); //0, 1, 2;
+  console.log(newNames); //new array of names;
+  return newNames;
+}
+
+names(['James', 'Brad', 'Jenny']);
+
+function names(names) {
+  let newNames = [];
+  for(let i = 0; i < names.length; i++) {
+    const name = names[i];
+    newNames.push(`my name is ${name}`);
+  }
+  console.log(i); //undefined
+  console.log(newNames);
+  return newNames;
+}
+
+names(['James', 'Brad', 'Jenny']);
+
+//const is very similar to let, however, the big difference is when you assign a value to a variable using const you cannot reassign it to a new value;
+
+let username = 'Tyler'
+const email = 'tylermcginnis@gmail.com'
+
+name = 'Tyler McGinnis' //
+handle = 'tylermcginnis12@gmail.com' // TypeError: Assignment to constant variable.
+
+
+//objects
+
+const obj = {
+  name: 'tyler'
+};
+
+obj.name = 'jared'; //all good
+
+obj = {}; // nope
+
+
+/* additional info
+var:
+  function scoped
+  undefined when accessing a variable before it's declared
+
+let:
+  block scoped
+  ReferenceError when accessing a variable before it's declared
+
+const:
+  block scoped
+  ReferenceError when accessing a variable before it's declared
+  can't be reassigned
+*/
