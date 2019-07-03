da3d9da78fe6a3605f88ed90847e2f80ccb1af2b
Mackenzie Berliner-Glasser <mack.bg94@gmail.com>
2019-07-03 12:09:39 -0700

    intro to generators

---
+++ src/generators/for-of-loops.js
@@ -0,0 +1,15 @@
+const dogs = ['james', 'peet', 'chance'];
+
+// just another way to get into an array of data!
+for(let dog of dogs) {
+  console.log(dog);
+}
+
+const numbers = [1, 2, 3, 4];
+let total = 0;
+for(let number of numbers) {
+  total += number;
+}
+
+console.log(total);
+
